4cf7092e1ffc66346ffadfbd1555f8fdf29702df
Bram Bekkers <bram.bekkers@nl.abnamro.com>
2024-09-12 11:18:02 +0200

    small changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true, // also necessary
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
