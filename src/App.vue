3e932320e4d87f30eb181c5999b98e219334aec5
Bram Bekkers <bram.bekkers@nl.abnamro.com>
2024-09-12 09:50:31 +0200

    added screenshot to bg

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>test</main>
+  <main class="relative h-screen w-screen">
+    <img class="absolute inset-0 object-cover" src="./assets/background.jpg" alt="cool abn background" />
+  </main>
 </template>
