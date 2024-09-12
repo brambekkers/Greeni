aa133782d48c877a3339f87903680df07b10ef39
Jeffrey Meesters <jeffrey.meesters@nl.abnamro.com>
2024-09-12 19:43:20 +0200

    rename prompt store and implement fetching data on Main.vue

--- src/stores/prompt.ts
+++ src/stores/prompt.ts
@@ -1,11 +1,13 @@
+import { ref } from 'vue';
 import { defineStore } from 'pinia';
 import { useAxios } from '../utils/request';
 
-export const useGreeniStore = defineStore('greeni', () => {
+export const usePromptStore = defineStore('Prompt', () => {
+  const genericScoreAnswer = ref();
+
   async function getGenericScore(transactions) {
     try {
-      const prompt = `
-        Using the provided transaction array generate JSON output that calculates a green score for each transaction and category (groceries, dining, and transport). 
+      const prompt = `Using the provided transaction array generate JSON output that calculates a green score for each transaction and category (groceries, dining, and transport). 
         Base the green score on a scale of 1 to 10. 
         Per category I would like a total of the transactions, total spent and an average green score.
   
@@ -15,12 +17,14 @@ export const useGreeniStore = defineStore('greeni', () => {
       `;
       const promptAnswer = await useAxios('GET', 'url', { prompt });
       console.log(promptAnswer);
+      genericScoreAnswer.value = promptAnswer;
     } catch (error) {
       console.log(error);
     }
   }
 
   return {
+    genericScoreAnswer,
     getGenericScore,
   };
 });
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,19 +1,33 @@
 <script setup>
+import { watch } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useRoute } from 'vue-router';
+import { useStatementstore } from '../stores/statements';
+import { usePromptStore } from '../stores/prompt';
 import QuestionForm from '@/components/questionForm/QuestionForm.vue';
 import Outcome from '@/components/Outcome.vue';
 import Greeni from '@/components/Greeni.vue';
-import { useStatementstore } from '../stores/statements';
-import { useRoute } from 'vue-router';
 
-const { statements, fetchStatements } = useStatementstore();
+const { statements, fetchStatements } = storeToRefs(useStatementstore());
+const { getGenericScore, genericScoreAnswer } = storeToRefs(usePromptStore());
 
 const route = useRoute();
-fetchStatements(route.params?.person?.[0]);
+await fetchStatements(route.params?.person?.[0]);
+
+watch(statements, async () => {
+  await usePromptStore(statements);
+});
+
+console.log(genericScoreAnswer);
 </script>
 
 <template>
   <main class="relative h-screen w-screen overflow-hidden">
-    <img class="absolute inset-0 object-cover z-0 w-full" src="../assets/background.png" alt="Cool abn background" />
+    <img
+      class="absolute inset-0 object-cover z-0 w-full"
+      src="../assets/background.png"
+      alt="Cool abn background"
+    />
     <div class="relative z-10">
       <QuestionForm />
       <Outcome />
