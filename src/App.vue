5d325a0eb1c59f87685af8bbf708bf4b19ed3b4f
Bram Bekkers <bram.bekkers@nl.abnamro.com>
2024-09-12 09:51:10 +0200

    added additional styling

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="relative h-screen w-screen">
-    <img class="absolute inset-0 object-cover" src="./assets/background.jpg" alt="cool abn background" />
+  <main class="relative h-screen w-screen overflow-hidden">
+    <img class="absolute inset-0 object-cover" src="./assets/background.jpg" alt="Cool abn background" />
   </main>
 </template>
