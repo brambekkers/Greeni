c0fc23e76b7b1a4c6a66368fbc96c98e776c405e
Ali Gok <ali.goek@nl.abnamro.com>
2024-09-12 21:10:55 +0200

    stored questions in store

--- src/components/questionForm/Stepper.vue
+++ src/components/questionForm/Stepper.vue
@@ -13,42 +13,30 @@ import Divider from 'primevue/divider';
 import SelectButton from 'primevue/selectbutton';
 import RadioButton from 'primevue/radiobutton';
 
+import {useAnswersStore} from "@/stores/answers"
+const { 
+    optionOneFirstQuestion,
+    optionTwoFirstQuestion,
+    optionThreeFirstQuestion,
+    optionFourFirstQuestion,
+    optionFiveFirstQuestion,
+    valueSecondQuestion,
+    optionsSecondQuestion,
+    valueThirdQuestion,
+    optionsThirdQuestion,
+    valueFourthQuestion,
+    optionsFourthQuestion,
+    valueFifthQuestion,
+    optionsFifthQuestion,
+    valueSixthQuestion,
+    optionsSixthQuestion,
+  } = storeToRefs(useAnswersStore());
+
+
 defineEmits(['submit']);
 
 const activeStep = ref(1);
 
-//First question
-const optionOneFirstQuestion = ref(false);
-const optionTwoFirstQuestion = ref(false);
-const optionThreeFirstQuestion = ref(false);
-const optionFourFirstQuestion = ref(false);
-const optionFiveFirstQuestion = ref(false);
-
-//Second question
-
-const valueSecondQuestion = ref('House');
-const optionsSecondQuestion = ref(['House', 'Apartment']);
-
-//Third Question
-const valueThirdQuestion = ref('Weekly');
-const optionsThirdQuestion = ref([
-  { name: 'Daily', key: 'Daily' },
-  { name: 'Weekly', key: 'Weekly' },
-  { name: 'Bi-weekly', key: 'Bi-weekly' },
-  { name: 'Monthly', key: 'Monthly' },
-]);
-
-// Fourth Question
-const valueFourthQuestion = ref("I'm planning to do!");
-const optionsFourthQuestion = ref(['Yes, I do!', "I'm planning to do!", 'No, not my thing!']);
-
-// Fifth Question
-const valueFifthQuestion = ref('Somewhat conscious');
-const optionsFifthQuestion = ref(['Very conscious', 'Somewhat conscious', 'Not very conscious']);
-
-// Sixth Question
-const valueSixthQuestion = ref("I'm planning to do!");
-const optionsSixthQuestion = ref(['Yes, I do!', "I'm planning to do!", 'No, not my thing!']);
 </script>
 
 <template>
