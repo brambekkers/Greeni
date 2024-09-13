f0b191caf01c9488b547f3df5c4ffb412e49615f
Bram Bekkers <bram.bekkers@nl.abnamro.com>
2024-09-13 09:34:37 +0200

    Update some components

--- src/components/outcome/Overview.vue
+++ src/components/outcome/Overview.vue
@@ -1,9 +1,10 @@
-<template>
-  <Card>
-    <template #content> test</template>
-  </Card>
-</template>
-
 <script setup lang="ts">
 import Card from 'primevue/card';
 </script>
+
+<template>
+  <Card>
+    <template #title>Simple Card</template>
+    <template #content>Lorem</template>
+  </Card>
+</template>
