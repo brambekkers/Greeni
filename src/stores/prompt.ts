f98a9905fd014ceb6e352b0d271ac9f6b044d33b
Jeffrey Meesters <jeffrey.meesters@nl.abnamro.com>
2024-09-13 09:22:44 +0200

    Update prompt and add scoredata to persona data

--- src/stores/prompt.ts
+++ src/stores/prompt.ts
@@ -7,10 +7,10 @@ export const usePromptStore = defineStore('Prompt', () => {
 
   async function getGenericScore(transactions) {
     try {
-      const prompt = `Using the provided TransactionArray generate JSON output that calculates a green score for each transaction and category (groceries, dining, and transport). Base the green score on a scale of 1 to 10. Per category I would like a total of the transactions, total spent and an average green score and an average green score for all categories combined. ${transactions.value}`;
+      const prompt = `Using the provided JSON below generate new JSON output that calculates a green score for each category: groceries, dining, and transport. Base the green score on a scale of 1 to 10. Per category I would like a total of the transactions, an array of the transaction_id of the transactions, a total spent and an average green score and also an average green score for all categories combined. Please only provide the newly generated JSON and not an explanation. \`\`\`JSON ${transactions.value} \`\`\``;
       const promptAnswer = await useAxios(
         'POST',
-        'https://digitaldreamgreenie-g9cqhzfaardxbfen.westeurope-01.azurewebsites.net/api/openai/generic',
+        'https://digitaldreamgreenie-g9cqhzfaardxbfen.westeurope-01.azurewebsites.net/api/openai/generate',
         { prompt }
       );
 
