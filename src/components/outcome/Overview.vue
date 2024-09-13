d18c4a0c4ce075fae5770592405afc80408c37fa
Bram Bekkers <bram.bekkers@nl.abnamro.com>
2024-09-13 09:49:23 +0200

    Store results parmas map

--- src/components/outcome/Overview.vue
+++ src/components/outcome/Overview.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import Card from 'primevue/card';
+
+import { useResultStore } from '@/stores/result';
+import { storeToRefs } from 'pinia';
+
+const { resultPerson } = storeToRefs(useResultStore());
 </script>
 
 <template>
   <Card>
-    <template #title>Simple Card</template>
-    <template #content>Lorem</template>
+    <template #title>
+      <h3>Total overview</h3>
+      <p>Total 48.5% Growth 😎 this month</p>
+    </template>
+    <template #content>{{ resultPerson }}</template>
   </Card>
 </template>
--- src/stores/result.ts
+++ src/stores/result.ts
@@ -1,8 +1,30 @@
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+
 import { defineStore } from 'pinia';
+import { useRoute } from 'vue-router';
+
+// data
+import DideData from '../assets/data/dide/greenScoreData.json';
+import SanderData from '../assets/data/sander/greenScoreData.json';
+import SophiaData from '../assets/data/sophia/greenScoreData.json';
 
 export const useResultStore = defineStore('result', () => {
-  const visible = ref(false);
+  const visible = ref(true);
 
-  return { visible };
+  const route = useRoute();
+
+  const resultPerson = computed(() => {
+    const person = route.params?.person?.[0] || ('dide' as string);
+    const personlowercase = person?.toLowerCase();
+
+    if (personlowercase === 'dide') {
+      return DideData;
+    } else if (personlowercase === 'sander') {
+      return SanderData;
+    } else if (personlowercase === 'sophia') {
+      return SophiaData;
+    }
+  });
+
+  return { visible, resultPerson };
 });
